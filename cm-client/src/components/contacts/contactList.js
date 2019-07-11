eff0755c687e90cb826731591eb74fd8c1f57e37
komalshivani <komalshivani2@gmail.com>
2019-07-11 16:21:53 -0400

    added id at response.data._id

--- cm-client/src/components/contacts/contactList.js
+++ cm-client/src/components/contacts/contactList.js
@@ -1,45 +1,55 @@
-import React from 'react'
-import axios from '../../config/axios-config'
-import {Link} from 'react-router-dom'
-import {connect} from 'react-redux'
-import {removeContact} from '../../action/a-contacts'
+import React from "react";
+import axios from "../../config/axios-config";
+import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { removeContact } from "../../action/a-contacts";
 
-class ContactList extends React.Component{  
-   handleRemove=(contact)=>{
-        console.log(contact)
-        const confirmRemove=window.confirm('Are You Sure?')
-        if(confirmRemove){
-            axios.delete(`/contacts/${contact._id}`,{
-                headers: {
-                    'x-auth':localStorage.getItem('userAuthToken')
-                }
-            })
-            .then(response=>{
-                this.props.dispatch(removeContact(response.data))
-            })
-        }
+class ContactList extends React.Component {
+  handleRemove = contact => {
+    console.log(contact);
+    const confirmRemove = window.confirm("Are You Sure?");
+    if (confirmRemove) {
+      axios
+        .delete(`/contacts/${contact._id}`, {
+          headers: {
+            "x-auth": localStorage.getItem("userAuthToken")
+          }
+        })
+        .then(response => {
+          this.props.dispatch(removeContact(response.data._id));
+        });
     }
-    render(){
-        return(
-            <div>
-                <h3>Listing Contacts:{this.props.contacts.length}</h3>
-                <ul>
-                    { this.props.contacts.map((contact)=>{
-                        return <li key={contact._id}>
-                            <Link to={`/contacts/${contact._id}`}>{contact.name}</Link><button onClick={()=>{this.handleRemove(contact)}}>Delete</button>
-                        </li>
-                    })}      
-                </ul>
-                <Link to="/contacts/new">Add contacts</Link> 
-            </div>
-        )
-    }
+  };
+  render() {
+    return (
+      <div>
+        <h3>Listing Contacts:{this.props.contacts.length}</h3>
+        <ul>
+          {this.props.contacts.map(contact => {
+            return (
+              <li key={contact._id}>
+                <Link to={`/contacts/${contact._id}`}>{contact.name}</Link>
+                <button
+                  onClick={() => {
+                    this.handleRemove(contact);
+                  }}
+                >
+                  Delete
+                </button>
+              </li>
+            );
+          })}
+        </ul>
+        <Link to="/contacts/new">Add contacts</Link>
+      </div>
+    );
+  }
 }
 
-const mapStateToProps=(state)=>{
-    return{
-        user:state.user,
-        contacts:state.contacts
-    }
-}
-export default connect(mapStateToProps)(ContactList)
+const mapStateToProps = state => {
+  return {
+    user: state.user,
+    contacts: state.contacts
+  };
+};
+export default connect(mapStateToProps)(ContactList);
