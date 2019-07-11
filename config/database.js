924a0f647ad4e9918715109f4e71221273107d5f
komalshivani <komalshivani2@gmail.com>
2019-07-11 17:46:14 -0400

    deploying contact-manager

--- config/database.js
+++ config/database.js
@@ -2,7 +2,7 @@ const mongoose=require('mongoose')
 
 mongoose.Promise=global.Promise
 mongoose.set('useCreateIndex', true)
-const connection_uri = process.env.MONGODB_URI || 'mongodb://localhost:27017/contact-manager-app'
+const connection_uri = process.env.MONGODB_URI || 'mongodb+srv://komal-shivani:9980445081ks@cluster0-kduxw.mongodb.net/test?retryWrites=true&w=majority'
 mongoose.connect(connection_uri, {
         useNewUrlParser: true
     })
@@ -13,5 +13,4 @@ mongoose.connect(connection_uri, {
 .catch((err)=>{
     console.log('error connecting to db')
 })
-
 module.exports=mongoose
